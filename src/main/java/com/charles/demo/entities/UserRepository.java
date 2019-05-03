e5a290db0d72afe2fe9ce0733b9a952d814df71d
dongchao <dongchao@bigo.sg>
2019-05-03 12:59:41 +0800

    jpa使用

---
+++ src/main/java/com/charles/demo/entities/UserRepository.java
@@ -0,0 +1,18 @@
+package com.charles.demo.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * JpaRepository实现了基本的查询功能
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name")
+    User findUser(@Param("name") String name);
+}
